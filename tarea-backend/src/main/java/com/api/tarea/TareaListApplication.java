57554264087040d3b5dbfabdceac66adc53123e4
Ruben Alexander <rubenalexander644@outlook.es>
2024-07-25 16:47:30 -0400

    init: Tareas list

---
+++ tarea-backend/src/main/java/com/api/tarea/TareaListApplication.java
@@ -0,0 +1,13 @@
+package com.api.tarea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaListApplication.class, args);
+	}
+
+}
